dbe02ac4e38c2598a566dd7526d62fd09050e4f8
develoFavour <opiafavour10@gmail.com>
2024-06-21 20:56:10 +0300

    commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Testimonials", link: "#testimonials" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: "I prioritize client collaboration, fostering open communication ",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Currently building a JS Animation library",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "PINCIAL",
+		des: "Explore and Share your experience with my picture sharing app all with React, Tailwind CSS and Sanity for Content Management.",
+		img: "/pincial.jpeg",
+		iconLists: ["/re.svg", "/tail.svg"],
+		link: "https://github.com/adrianhajdin?tab=repositories",
+	},
+	{
+		id: 2,
+		title: "News Reel: 24hr News App ",
+		des: "A 24/7 Live News App that gives you the most recent and quality news.",
+		img: "/newsreel.png",
+		iconLists: ["/re.svg", "/tail.svg"],
+		link: "/ui.yoom.com",
+	},
+	{
+		id: 3,
+		title: "Spotify Clone",
+		des: "The Most Famous Music Streaming Platform cloned by me in React.",
+		img: "/spotify-logo2.png",
+		iconLists: ["/re.svg", "/tail.svg"],
+		link: "/ui.yoom.com",
+	},
+	{
+		id: 4,
+		title: "Netflix Clone",
+		des: "Recreated the famous Netflix Movie streaming platform website..",
+		img: "/netflix.svg",
+		iconLists: ["/res.svg", "/tail.svg"],
+		link: "/ui.apple.com",
+	},
+];
+
+export const testimonials = [
+	{
+		quote:
+			"Collaborating with Favour Opia was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Favour Opia's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Favour Opia is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Favour Opia was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Favour Opia's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Favour Opia is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Favour Opia was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Favour Opia's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Favour Opia is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Favour Opia was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Favour Opia's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Favour Opia is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote:
+			"Collaborating with Favour Opia was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Favour Opia's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Favour Opia is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: "cloudinary",
+		img: "/cloud.svg",
+		nameImg: "/cloudName.svg",
+	},
+	{
+		id: 2,
+		name: "appwrite",
+		img: "/app.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 3,
+		name: "HOSTINGER",
+		img: "/host.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 4,
+		name: "stream",
+		img: "/s.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "docker.",
+		img: "/dock.svg",
+		nameImg: "/dockerName.svg",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "HTML, CSS and JavaScript Intern",
+		desc: "Started my web development journey as an HTML, CSS and JavaScript intern, built amazing websites with these technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 2,
+		title: "React Developer",
+		desc: "Took my we development skills up a notch, mastered the art of programming in the most famous javascript framework.",
+		className: "md:col-span-2",
+		thumbnail: "/exp2.svg",
+	},
+	{
+		id: 3,
+		title: "Freelance React Developer Project",
+		desc: "Led the dev of a web application for a client, from initial concept to deployment.",
+		className: "md:col-span-2",
+		thumbnail: "/exp3.svg",
+	},
+	{
+		id: 4,
+		title: "Lead Frontend Developer",
+		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		className: "md:col-span-2",
+		thumbnail: "/exp4.svg",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+	},
+	{
+		id: 2,
+		img: "/twit.svg",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+	},
+];
